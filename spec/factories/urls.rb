20ad7cb2ea730f091f9b591edebf5572f63b91e0
Steve Forse <steve.forse@gmail.com>
2025-04-04 12:25:33 +0700

    Add a representative factory for the model

---
+++ spec/factories/urls.rb
@@ -0,0 +1,21 @@
+FactoryBot.define do
+  factory :url do
+    url { "https://opengraphexamples.com/examples/hunted.space-product-pages/" }
+    status { "success" }
+    title { "Open Graph Examples" }
+    description { "Advice, helpful content, and tools to make your Open Graph social cards stand out." }
+    object_type { "website" }
+    image_url { "https://opengraphexamples.com/og.jpg" }
+    canonical_url { "https://opengraphexamples.com" }
+    metadata do
+      {
+        "type" => "website",
+        "url" => "https://opengraphexamples.com",
+        "title" => "Open Graph Examples",
+        "description" => "Advice, helpful content, and tools to make your Open Graph social cards stand out.",
+        "image" => "https://opengraphexamples.com/og.jpg"
+      }
+    end
+    most_recent_error { nil }
+  end
+end
